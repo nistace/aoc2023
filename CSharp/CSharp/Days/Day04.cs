ce3a7193488c9b4e2dae559ad040dcdaee29154f
Nathan Istace <nathan.istace@gmail.com>
2023-12-04 09:43:02 +0100

    C# Day 4 Part 2

--- CSharp/CSharp/Days/Day04.cs
+++ CSharp/CSharp/Days/Day04.cs
@@ -15,7 +15,17 @@ namespace AOC23.Days {
 		}
 
 		public static string Part2() {
-			return $"";
+			var input = Utils.ReadAllLines(4).Where(t => !string.IsNullOrEmpty(t));
+			var lineNumbers = input.Select(t => inputLineRegex.Match(t)).Select(t => (winning: GetListOfNumbers(t.Groups["winning"].Value), played: GetListOfNumbers(t.Groups["played"].Value))).ToArray();
+			var scores = lineNumbers.Select(t => t.played.Count(p => t.winning.Contains(p))).ToArray();
+			var numberOfCards = Enumerable.Repeat(1, lineNumbers.Length).ToArray();
+			for (var i = 0; i < lineNumbers.Length; ++i) {
+				for (var j = 0; j < scores[i]; ++j) {
+					numberOfCards[i + j + 1] += numberOfCards[i];
+				}
+			}
+
+			return $"{numberOfCards.Sum()}";
 		}
 
 		private static IEnumerable<int> GetListOfNumbers(string stringOfNumbers) => stringOfNumbers.Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse);
