2b9182cba68e91921376da6100062d81eaaf388d
Nathan Istace <nathan.istace@gmail.com>
2023-12-05 08:04:53 +0100

    C# Day 5 Part 2

--- CSharp/CSharp/Days/Day05.cs
+++ CSharp/CSharp/Days/Day05.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -14,32 +13,57 @@ namespace AOC23.Days {
 		}
 
 		public static string Part2() {
-			return $"";
+			var inputLines = Utils.ReadAllLines(5).ToArray();
+			var maps = Map.ParseAll(inputLines.Skip(1)).Reverse().ToArray();
+			var seedRanges = SeedRange.ParseAll(inputLines[0]);
+
+			for (var i = 0L;; i++) {
+				var source = maps.Aggregate(i, (cur, map) => map.ReverseConvert(cur));
+				if (seedRanges.Any(t => t.ContainsValue(source))) return $"{i}";
+			}
+		}
+
+		private class SeedRange {
+			private long Start { get; }
+			private long Length { get; }
+			private long End => Start + Length - 1;
+
+			public static IReadOnlyList<SeedRange> ParseAll(string inputSeedLine) {
+				var seedRanges = new List<SeedRange>();
+				var values = inputSeedLine.Replace("seeds:", "").Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(long.Parse).ToArray();
+				for (var i = 0; i < values.Length; i += 2) {
+					seedRanges.Add(new SeedRange(values[i], values[i + 1]));
+				}
+				return seedRanges;
+			}
+
+			private SeedRange(long start, long length) {
+				Start = start;
+				Length = length;
+			}
+
+			public bool ContainsValue(long value) => value >= Start && value <= End;
 		}
 
 		private class Map {
 			private static Regex mapTitleRegex { get; } = new Regex("\\w+-to-(?<dest>\\w+) +map:");
 
-			public string Destination { get; }
 			private IReadOnlyList<Conversion> Conversions { get; }
 
-			private Map(string destination, IEnumerable<Conversion> conversions) {
-				Destination = destination;
+			private Map(IEnumerable<Conversion> conversions) {
 				Conversions = conversions.ToArray();
 			}
 
 			public static IReadOnlyList<Map> ParseAll(IEnumerable<string> inputMapLines) {
 				var maps = new List<Map>();
-				var currentMapDestination = string.Empty;
 				var currentMapConversionArray = new List<Conversion>();
 				foreach (var inputLine in inputMapLines) {
 					var mapTitleMatch = mapTitleRegex.Match(inputLine);
 					if (mapTitleMatch.Success) {
 						if (currentMapConversionArray.Count > 0) {
-							maps.Add(new Map(currentMapDestination, currentMapConversionArray));
+							maps.Add(new Map(currentMapConversionArray));
 							currentMapConversionArray.Clear();
 						}
-						currentMapDestination = mapTitleMatch.Groups["dest"].Value;
 						continue;
 					}
 					if (Conversion.Parse(inputLine, out var conversion)) {
@@ -47,19 +71,32 @@ namespace AOC23.Days {
 					}
 				}
 				if (currentMapConversionArray.Count > 0) {
-					maps.Add(new Map(currentMapDestination, currentMapConversionArray));
+					maps.Add(new Map(currentMapConversionArray));
 				}
 				return maps;
 			}
 
-			public long Convert(long value) => Conversions.FirstOrDefault(t => t.ContainsSource(value))?.Convert(value) ?? value;
+			public long Convert(long value) {
+				foreach (var conversion in Conversions) {
+					if (conversion.ContainsSource(value)) return conversion.Convert(value);
+				}
+				return value;
+			}
+
+			public long ReverseConvert(long value) {
+				foreach (var conversion in Conversions) {
+					if (conversion.ContainsDestination(value)) return conversion.ReverseConvert(value);
+				}
+				return value;
+			}
 
 			private class Conversion {
 				private static Regex regex { get; } = new Regex("(?<dest>\\d+) +(?<src>\\d+) +(?<length>\\d+)");
 				private long SourceRangeStart { get; }
-				private long DestinationRangeStart { get; }
-				private long RangeLength { get; }
 				private long SourceRangeEnd => SourceRangeStart + RangeLength - 1;
+				private long DestinationRangeStart { get; }
+				private long DestinationRangeEnd => DestinationRangeStart + RangeLength - 1;
+				private long RangeLength { get; }
 
 				private Conversion(long sourceRangeStart, long destinationRangeStart, long rangeLength) {
 					SourceRangeStart = sourceRangeStart;
@@ -81,7 +118,14 @@ namespace AOC23.Days {
 					return value - SourceRangeStart + DestinationRangeStart;
 				}
 
+				public long ReverseConvert(long value) {
+					if (value < DestinationRangeStart) return value;
+					if (value > DestinationRangeEnd) return value;
+					return value + SourceRangeStart - DestinationRangeStart;
+				}
+
 				public bool ContainsSource(long value) => value >= SourceRangeStart && value <= SourceRangeEnd;
+				public bool ContainsDestination(long value) => value >= DestinationRangeStart && value <= DestinationRangeEnd;
 			}
 		}
 	}
