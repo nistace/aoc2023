3d965dc273668cf25c53a113b1424a83678f66b6
Nathan Istace <nathan.istace@gmail.com>
2023-12-06 18:54:56 +0100

    C# Day 6 part 2

--- CSharp/CSharp/Days/Day06.cs
+++ CSharp/CSharp/Days/Day06.cs
@@ -6,12 +6,24 @@ namespace AOC23.Days {
 			var input = Utils.ReadAllLines(6).ToArray();
 			var times = input[0].Replace("Time:", "").Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToArray();
 			var distances = input[1].Replace("Distance:", "").Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToArray();
-			var numberOfWaysToBeat = times.Select((t, i) => Enumerable.Range(0, t + 1).Count(x => t * x - x * x > distances[i])).ToArray();
+			var numberOfWaysToBeat = times.Select((t, i) => Enumerable.Range(0, t + 1).Count(x => IsBetterScore(x, t, distances[i]))).ToArray();
 			return $"{numberOfWaysToBeat.Aggregate(1, (cur, next) => cur * next)}";
 		}
 
 		public static string Part2() {
-			return "";
+			var input = Utils.ReadAllLines(6).ToArray();
+			var time = long.Parse(input[0].Replace("Time:", "").Replace(" ", ""));
+			var distance = long.Parse(input[1].Replace("Distance:", "").Replace(" ", ""));
+
+			var min = 0;
+			var max = time;
+
+			while (!IsBetterScore(min, time, distance)) min++;
+			while (!IsBetterScore(max, time, distance)) max--;
+
+			return $"{max - min + 1}";
 		}
+
+		private static bool IsBetterScore(long x, long time, long distance) => time * x - x * x > distance;
 	}
 }
