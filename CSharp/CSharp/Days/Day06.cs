d000ae54cbcc960ae59f38debacca95064ac6901
Nathan Istace <nathan.istace@gmail.com>
2023-12-06 08:21:38 +0100

    Day 6

---
+++ CSharp/CSharp/Days/Day06.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace AOC23.Days {
+	public static class Day06 {
+		public static string Part1() {
+			var input = Utils.ReadAllLines(6).ToArray();
+			var times = input[0].Replace("Time:", "").Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToArray();
+			var distances = input[1].Replace("Distance:", "").Split(' ').Where(t => !string.IsNullOrEmpty(t)).Select(int.Parse).ToArray();
+			var numberOfWaysToBeat = times.Select((t, i) => Enumerable.Range(0, t + 1).Count(x => t * x - x * x > distances[i])).ToArray();
+			return $"{numberOfWaysToBeat.Aggregate(1, (cur, next) => cur * next)}";
+		}
+
+		public static string Part2() {
+			return "";
+		}
+	}
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,8 +4,8 @@ using AOC23.Days;
 namespace AOC23 {
 	public static class Program {
 		private static void Main() {
-			Console.WriteLine($"Part 1: {Day05.Part1()}");
-			Console.WriteLine($"Part 2: {Day05.Part2()}");
+			Console.WriteLine($"Part 1: {Day06.Part1()}");
+			Console.WriteLine($"Part 2: {Day06.Part2()}");
 		}
 	}
 }
