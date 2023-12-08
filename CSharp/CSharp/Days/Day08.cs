b240cb20fa8b1d5075314e78150d13792e22b117
Nathan Istace <nathan.istace@gmail.com>
2023-12-08 19:55:59 +0100

    C# Day 8 Part 2

--- CSharp/CSharp/Days/Day08.cs
+++ CSharp/CSharp/Days/Day08.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AOC23.Days {
@@ -15,8 +16,24 @@ namespace AOC23.Days {
 		}
 
 		public static string Part2() {
-			return "";
+			var inputLines = Utils.ReadAllLines(8).ToArray();
+			var directions = inputLines[0].Where(t => t == 'L' || t == 'R').Select(t => (t, t == 'L' ? (Func<(string, string), string>)Left : Right)).ToArray();
+			var nodes = inputLines.Skip(1).Where(t => !string.IsNullOrEmpty(t)).Select(ParseNodeInputLine).ToDictionary(t => t.from, t => t.dests);
+
+			var cycleLengthBetweenEndingNodes = nodes.Keys.Where(t => t.EndsWith('Z')).ToDictionary(t => t, t => 0L);
+			foreach (var endingNode in nodes.Keys.Where(t => t.EndsWith('Z'))) {
+				var nextNode = endingNode;
+				do {
+					nextNode = directions[cycleLengthBetweenEndingNodes[endingNode]++ % directions.Length].Item2(nodes[nextNode]);
+				} while (!nextNode.EndsWith('Z'));
+			}
+
+			return $"{LCM(cycleLengthBetweenEndingNodes.Values)}";
 		}
+
+		private static long LCM(IEnumerable<long> numbers) => numbers.Aggregate(LCM);
+		private static long LCM(long a, long b) => Math.Abs(a * b) / GCD(a, b);
+		private static long GCD(long a, long b) => b == 0 ? a : GCD(b, a % b);
 
 		private static (string from, (string left, string right) dests) ParseNodeInputLine(string inputLine) =>
 			(inputLine.Substring(0, 3), (inputLine.Substring(inputLine.IndexOf('(') + 1, 3), inputLine.Substring(inputLine.IndexOf(',') + 2, 3)));
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using AOC23.Days;
 
 namespace AOC23 {
 	public static class Program {
 		private static void Main() {
-			Console.WriteLine($"Part 1: {Day07.Part1()}");
-			Console.WriteLine($"Part 2: {Day07.Part2()}");
+			Console.WriteLine($"Part 1: {Day08.Part1()}");
+			Console.WriteLine($"Part 2: {Day08.Part2()}");
 		}
-
 	}
 }
