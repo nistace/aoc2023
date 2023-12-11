7982a963acc242ea4e09608e450c3f2f0c8fe280
Nathan Istace <nathan.istace@gmail.com>
2023-12-11 12:02:20 +0100

    C# Day 11 Part 2

--- CSharp/CSharp/Days/Day11.cs
+++ CSharp/CSharp/Days/Day11.cs
@@ -1,65 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AOC23.Days {
 	public static class Day11 {
 		public static string Part1() {
 			var galaxies = ParseInput(Utils.ReadAllLines(11));
-			Expand(galaxies);
-
-			var lengthSum = 0;
-			for (var i = 0; i < galaxies.Count; i++) {
-				for (var j = i + 1; j < galaxies.Count; j++) {
-					lengthSum += Math.Abs(galaxies[i].x - galaxies[j].x) + Math.Abs(galaxies[i].y - galaxies[j].y);
-				}
-			}
-
-			return $"{lengthSum}";
+			EvaluateRowAndColumnSizes(galaxies, 2, out var rowSizes, out var columnSizes);
+			return $"{EvaluateSumOfDistances(galaxies, columnSizes, rowSizes)}";
 		}
 
 		public static string Part2() {
-			return $"";
+			var galaxies = ParseInput(Utils.ReadAllLines(11));
+			EvaluateRowAndColumnSizes(galaxies, 1000000, out var rowSizes, out var columnSizes);
+			return $"{EvaluateSumOfDistances(galaxies, columnSizes, rowSizes)}";
 		}
 
-		private static void Display(IEnumerable<(int x, int y)> galaxies) {
-			var asArray = galaxies as (int x, int y)[] ?? galaxies.ToArray();
-			var width = asArray.Max(t => t.x);
-			var height = asArray.Max(t => t.y);
-
-			for (var y = 0; y <= height; ++y) {
-				for (var x = 0; x <= width; ++x) {
-					Console.Write(asArray.Contains((x, y)) ? '#' : '.');
+		private static BigInteger EvaluateSumOfDistances(List<(int x, int y)> galaxies, IReadOnlyList<int> columnSizes, IReadOnlyList<int> rowSizes) {
+			var lengthSum = new BigInteger(0);
+			for (var i1 = 0; i1 < galaxies.Count; i1++) {
+				for (var i2 = i1 + 1; i2 < galaxies.Count; i2++) {
+					lengthSum += Enumerable.Range(Math.Min(galaxies[i1].x, galaxies[i2].x), Math.Abs(galaxies[i1].x - galaxies[i2].x)).Sum(t => columnSizes[t]);
+					lengthSum += Enumerable.Range(Math.Min(galaxies[i1].y, galaxies[i2].y), Math.Abs(galaxies[i1].y - galaxies[i2].y)).Sum(t => rowSizes[t]);
 				}
-				Console.WriteLine();
 			}
+			return lengthSum;
+		}
+
+		private static void EvaluateRowAndColumnSizes(List<(int x, int y)> galaxies, int expendSize, out IReadOnlyList<int> rowSizes, out IReadOnlyList<int> columnSizes) {
+			columnSizes = Enumerable.Range(0, galaxies.Max(t => t.x) + 1).Select(t => galaxies.Any(u => u.x == t) ? 1 : expendSize).ToArray();
+			rowSizes = Enumerable.Range(0, galaxies.Max(t => t.y) + 1).Select(t => galaxies.Any(u => u.y == t) ? 1 : expendSize).ToArray();
 		}
 
 		private static List<(int x, int y)> ParseInput(IEnumerable<string> inputLines) =>
 			inputLines.SelectMany((row, y) => row.Select((c, x) => (c, x)).Where(t => t.c == '#').Select(t => (t.x, y))).ToList();
-
-		private static void Expand(this List<(int x, int y)> grid) {
-			var width = grid.Max(t => t.x);
-			for (var x = 0; x < width; ++x) {
-				if (grid.All(t => t.x != x)) {
-					width++;
-					x++;
-					var movedItems = grid.Where(t => t.x >= x).ToDictionary(t => t, t => (t.x + 1, t.y));
-					grid.RemoveAll(t => movedItems.ContainsKey(t));
-					grid.AddRange(movedItems.Values);
-				}
-			}
-
-			var height = grid.Max(t => t.y);
-			for (var y = 0; y < height; ++y) {
-				if (grid.All(t => t.y != y)) {
-					height++;
-					y++;
-					var movedItems = grid.Where(t => t.y >= y).ToDictionary(t => t, t => (t.x, t.y + 1));
-					grid.RemoveAll(t => movedItems.ContainsKey(t));
-					grid.AddRange(movedItems.Values);
-				}
-			}
-		}
 	}
 }
