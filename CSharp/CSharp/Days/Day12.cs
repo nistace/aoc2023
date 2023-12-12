8c6ea60866327ddefffe71e3adea58402a218812
Nathan Istace <nathan.istace@gmail.com>
2023-12-12 11:36:26 +0100

    C# Day 12 Part 2

--- CSharp/CSharp/Days/Day12.cs
+++ CSharp/CSharp/Days/Day12.cs
@@ -1,44 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AOC23.Days {
 	public static class Day12 {
+		private static Dictionary<string, BigInteger> scorePerSubInput { get; } = new Dictionary<string, BigInteger>();
+
 		public static string Part1() {
-			return $"{ParseInput(Utils.ReadAllLines(12)).Sum(t => CountPossibilities(t.sprintStates, 0, t.contiguousSizes, 0))}";
+			var inputLines = ParseInput(Utils.ReadAllLines(12));
+			var sum = inputLines.Select(t => CountPossibilities(t.springStates, t.contiguousSizes)).Aggregate(new BigInteger(0), (current, count) => current + count);
+			return $"{sum}";
 		}
 
 		public static string Part2() {
-			return "";
+			var inputLines = ParseInput(Utils.ReadAllLines(12));
+			var unfoldedInputLines = inputLines.Select(t => UnfoldInputLine(t, 5));
+			var sum = unfoldedInputLines.Select(t => CountPossibilities(t.springStates, t.contiguousSizes)).Aggregate(new BigInteger(0), (current, count) => current + count);
+			return $"{sum}";
 		}
 
-		private static IEnumerable<(string sprintStates, int[] contiguousSizes)> ParseInput(IEnumerable<string> inputLines) =>
+		private static (string springStates, int[] contiguousSizes) UnfoldInputLine((string springStates, int[] contiguousSizes) inputLine, int unfoldingLength) {
+			return (string.Join("?", Enumerable.Repeat(inputLine.springStates, unfoldingLength)),
+				Enumerable.Repeat(inputLine.contiguousSizes, unfoldingLength).Aggregate(Array.Empty<int>(), (cur, next) => cur.Concat(next).ToArray()));
+		}
+
+		private static IEnumerable<(string springStates, int[] contiguousSizes)> ParseInput(IEnumerable<string> inputLines) =>
 			inputLines.Where(t => !string.IsNullOrEmpty(t)).Select(t => (t.Split(' ')[0], t.Split(' ')[1].Split(',').Select(int.Parse).ToArray())).ToArray();
 
-		private static int CountPossibilities(string springStates, int springStateStartIndex, int[] contiguousSizes, int contiguousSizeStartIndex) {
-			if (contiguousSizeStartIndex >= contiguousSizes.Length) return springStates.LastIndexOf('#') >= springStateStartIndex ? 0 : 1;
-			var currentChunkSize = contiguousSizes[contiguousSizeStartIndex];
-			if (springStateStartIndex >= springStates.Length) return 0;
-			if (springStates.Length - springStateStartIndex < currentChunkSize) return 0;
-			if (springStates[springStateStartIndex] == '.') return CountPossibilities(springStates, springStateStartIndex + 1, contiguousSizes, contiguousSizeStartIndex);
-			if (springStates[springStateStartIndex] == '#') {
-				return CountPossibilitiesConsideringFirstAsDamaged(springStates, springStateStartIndex, contiguousSizes, contiguousSizeStartIndex);
+		private static BigInteger CountPossibilities(string springStates, IReadOnlyList<int> contiguousSizes) =>
+			CountPossibilities(springStates, 0, contiguousSizes, 0, contiguousSizes.Sum(t => t + 1) - 1);
+
+		private static BigInteger CountPossibilities(string springStates, int springStateStartIndex, IReadOnlyList<int> contiguousSizes, int contiguousSizeStartIndex, int minSpringStateLength) {
+			var subInput = springStates.Substring(springStateStartIndex) + " " + string.Join(",", contiguousSizes.Skip(contiguousSizeStartIndex));
+			if (scorePerSubInput.ContainsKey(subInput)) return scorePerSubInput[subInput];
+			var result = new BigInteger(0);
+			if (springStates.Length - springStateStartIndex < minSpringStateLength) result = 0;
+			else if (contiguousSizeStartIndex >= contiguousSizes.Count) result = springStates.LastIndexOf('#') >= springStateStartIndex ? 0 : 1;
+			else {
+				var currentChunkSize = contiguousSizes[contiguousSizeStartIndex];
+				if (springStateStartIndex >= springStates.Length) result = 0;
+				else if (springStates.Length - springStateStartIndex < currentChunkSize) result = 0;
+				else if (springStates[springStateStartIndex] == '.') result = CountPossibilities(springStates, springStateStartIndex + 1, contiguousSizes, contiguousSizeStartIndex, minSpringStateLength);
+				else if (springStates[springStateStartIndex] == '#') {
+					result = CountPossibilitiesConsideringFirstAsDamaged(springStates, springStateStartIndex, contiguousSizes, contiguousSizeStartIndex, minSpringStateLength);
+				}
+				else if (springStates[springStateStartIndex] == '?') {
+					result = CountPossibilities(springStates, springStateStartIndex + 1, contiguousSizes, contiguousSizeStartIndex, minSpringStateLength)
+								+ CountPossibilitiesConsideringFirstAsDamaged(springStates, springStateStartIndex, contiguousSizes, contiguousSizeStartIndex, minSpringStateLength);
+				}
 			}
-			if (springStates[springStateStartIndex] == '?') {
-				return CountPossibilities(springStates, springStateStartIndex + 1, contiguousSizes, contiguousSizeStartIndex)
-						+ CountPossibilitiesConsideringFirstAsDamaged(springStates, springStateStartIndex, contiguousSizes, contiguousSizeStartIndex);
-			}
-			throw new ArgumentException($"{springStates[springStateStartIndex]} is not a known state");
+			scorePerSubInput[subInput] = result;
+			return result;
 		}
 
-		private static int CountPossibilitiesConsideringFirstAsDamaged(string springStates, int springStateStartIndex, int[] contiguousSizes, int contiguousSizeStartIndex) {
+		private static BigInteger CountPossibilitiesConsideringFirstAsDamaged(string springStates, int springStateStartIndex, IReadOnlyList<int> contiguousSizes, int contiguousSizeStartIndex,
+			int minSpringStateLength) {
 			var currentChunkSize = contiguousSizes[contiguousSizeStartIndex];
 			for (var i = 1; i < currentChunkSize; ++i) {
 				if (springStates[springStateStartIndex + i] == '.') return 0;
 			}
-			if (springStates.Length - springStateStartIndex == currentChunkSize) return contiguousSizeStartIndex == contiguousSizes.Length - 1 ? 1 : 0;
+			if (springStates.Length - springStateStartIndex == currentChunkSize) return contiguousSizeStartIndex == contiguousSizes.Count - 1 ? 1 : 0;
 			if (springStates[springStateStartIndex + currentChunkSize] == '#') return 0;
-			return CountPossibilities(springStates, springStateStartIndex + currentChunkSize + 1, contiguousSizes, contiguousSizeStartIndex + 1);
+			return CountPossibilities(springStates, springStateStartIndex + currentChunkSize + 1, contiguousSizes, contiguousSizeStartIndex + 1, minSpringStateLength - currentChunkSize - 1);
 		}
 	}
 }
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,8 +4,16 @@ using AOC23.Days;
 namespace AOC23 {
 	public static class Program {
 		private static void Main() {
+			var watch = new System.Diagnostics.Stopwatch();
+			watch.Start();
 			Console.WriteLine($"Part 1: {Day12.Part1()}");
+			watch.Stop();
+			Console.WriteLine($"[Execution time: {watch.ElapsedMilliseconds / 1000f:0.000}s]");
+
+			watch.Restart();
 			Console.WriteLine($"Part 2: {Day12.Part2()}");
+			watch.Stop();
+			Console.WriteLine($"[Execution time: {watch.ElapsedMilliseconds / 1000f:0.000}s]");
 		}
 	}
 }
