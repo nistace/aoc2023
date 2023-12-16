c226e845d286bd0f31ea39895ebf7e85f3639d61
Nathan Istace <nathan.istace@gmail.com>
2023-12-16 15:19:21 +0100

    C# Day 15 Part 1

---
+++ CSharp/CSharp/Days/Day15.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC23.Days {
+	public static class Day15 {
+		public static string Part1() => $"{Utils.ReadAllLines(15).SelectMany(t => t.Split(",")).Sum(t => t.Aggregate(0L, (curr, next) => 17 * (curr + next) % 256))}";
+		public static string Part2() => "";
+	}
+}
